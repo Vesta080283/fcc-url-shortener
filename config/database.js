b327a3144ee61ecf2154dde023163624645cfc64
Lucas Rodrigues <lucas.rodrigues20@outlook.com>
2017-03-03 20:09:26 -0300

    database initial config

---
+++ config/database.js
@@ -0,0 +1,35 @@
+'use strict';
+
+const mongoose = require('mongoose');
+
+module.exports = function(){
+
+	const dbHost = process.env.DB_HOST || 'localhost';
+	const dbName = process.env.DB_NAME || 'url-shortner';
+
+	const uri = `mongodb://${dbHost}/${dbName}`;
+
+	mongoose.connect(uri);
+
+	mongoose.connection.on('connected', () => {
+		console.log(`Database connected to: ${uri}`);
+	});
+
+	mongoose.connection.on('disconnected', () => {
+		console.log(`Database disconnected from: ${uri}`);
+	});
+
+	mongoose.connection.on('error', err => {
+		console.log(`Database error on connection: ${err}`);
+	});
+
+	process.on('SIGINT', () => {
+
+		mongoose.connection.close(() => {
+			console.log('Database disconnected by the end of application');
+			process.exit(0);
+		});
+
+	});
+
+};
