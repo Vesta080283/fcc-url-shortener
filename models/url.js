c46e99cf14521fbced5ca386de39f67d5f417f95
Lucas Rodrigues <lucas.rodrigues20@outlook.com>
2017-03-03 20:12:54 -0300

    url mongoose model

---
+++ models/url.js
@@ -0,0 +1,20 @@
+'use strict';
+
+const mongoose = require('mongoose');
+const autoIncrement = require('mongoose-auto-increment');
+
+const urlSchema = mongoose.Schema({
+	originalUrl: {
+		type: String,
+		required: true,
+		unique: true
+	}
+}, {
+	timestamps: true
+});
+
+autoIncrement.initialize(mongoose.connection);
+urlSchema.plugin(autoIncrement.plugin, 'Url');
+const Url = mongoose.model('Url', urlSchema);
+
+module.exports = Url;
